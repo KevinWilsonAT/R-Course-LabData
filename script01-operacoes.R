f46c510bbd63ea37ebe632bcb89e02ccabba12c7
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-17 12:37:50 -0300

    upload classes 17-02-24 pt1

---
+++ script01-operacoes.R
@@ -0,0 +1,23 @@
+1+1                   # addition
+5-4                   # subtraction
+6*6                   # multiplication
+7/2                   # division
+(4*(6+2))/4           # parenthesis order
+4/0                   # showing infinity
+0/0                   # showing an error (NaN)
+pi                    # pi number
+(sin(pi/2))*3         # equation of sin and pi
+
+
+# Relational Operators:
+
+# greater             >
+# lesser              <
+# equal               ==
+# different           !=
+
+
+# attribution          =
+
+
+# remove variable      rm(<variable_name>)
---
+++ script02-logica-programacao.R
@@ -0,0 +1,36 @@
+# Logical Operators:
+
+# and                  
+# or                   
+# not                  !
+
+
+# simple condition
+
+# if(<condition>){
+#     ...
+# }
+
+
+# double condition
+
+# if(<condition>){
+#     ...
+# }
+# else{
+#     ...
+# }
+
+
+# nested condition
+
+# if(<condition>){
+#     run this block
+# }
+# else
+#     if(<condition2>) {
+#           run this block if <condition1> fails
+#     }
+#     else{
+#           run this block if all conditions fails
+#     }
