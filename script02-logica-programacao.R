8b48c81d1e9c4efa7d6de8d79a9e74f7de5aef8f
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-18 02:54:34 -0300

    Update script02-logica-programacao.R

--- script02-logica-programacao.R
+++ script02-logica-programacao.R
@@ -1,21 +1,19 @@
 # Logical Operators:
 
-# and                  
-# or                   
 # not                  !
 
 # -----------------------------------------------------------------------------
 
 # simple condition
 
-# if(<condition>){
+# if(condition){
 #     ...
 # }
 
 
 # double condition
 
-# if(<condition>){
+# if(condition){
 #     ...
 # }
 # else{
@@ -25,12 +23,12 @@
 
 # nested condition
 
-# if(<condition>){
+# if(condition1){
 #     run this block
 # }
 # else
-#     if(<condition2>) {
-#           run this block if <condition1> fails
+#     if(condition2) {
+#           run this block if condition1 fails
 #     }
 #     else{
 #           run this block if all conditions fails
@@ -40,21 +38,21 @@
 
 # looping structure
 
-# for (i in 1:4){  - This <1:4> will count from 1 to 4
-#      ...         - This will loop commands using the counter (<i>)
+# for (i in 1:4){  - This 1:4 will count from 1 to 4
+#      ...         - This will loop commands using the counter (i)
 # }
 
 # ------------------------------------------------------------------------------
 
 # Vector
 
-# <array_name> = (<field1>, <field2>, ... , <fieldN>) - numerical vector
+# vector_name = (field1, field2, ... , fieldN) - numerical vector
 
-# <array_name> = c(<string1>, <string2>, ... , <stringN>) - text vector
+# vector_name = c(string1, string2, ... , stringN) - text vector
 
 # To Access the vector, use:
 
-# for (i in <array_name>){
+# for (i in vector_name){
 #     ...
 # }
 
@@ -62,15 +60,15 @@
 
 # paste( ) function: using print, concatenates the contents of the array
 
-# syntax: print(paste(<i>, <string>, <separator>))
+# syntax: print(paste(i, string, separator))
 
 # parameters:
 
-#   <i>          = the position of the content in the array that you want
+#   i            = the position of the content in the array that you want
 #                  to concatenate
-#   <string>     = the string you want to concatenate with the string of
-#                  the position <i>
-#   <separator>  = separator between the strings
+#   string       = the string you want to concatenate with the string of
+#                  the position i
+#   separator    = separator between the strings
 
 # ------------------------------------------------------------------------------
 
@@ -78,42 +76,9 @@
 
 # with return and with input parameters
 
-# function(<a>, <b>){     = <a> and <b> are input parameters
+# function(a, b){     = a and b are input parameters
 #    ...
-#    return(<c>)          = <c> is the returning data of the function
+#    return(c)          = c is the returning data of the function
 # }
 
 # This type of function can return the data inside an variable
-
-
-# with return and without input parameters
-
-# function(){
-#    ...
-#    return(<c>)          = <c> is the returning data of the function
-# }
-
-# This type of function only returns data, there's no input parameters
-# This type of function can return the data inside an variable
-
-
-
-# without return and with input parameters
-
-# function(<a>, <b>){     = <a> and <b> are input parameters
-#    ...
-# }
-
-# Since there's no return in this function, the result of the function
-# probably will be used or show inside the function
-
-
-
-# without return and without input parameters
-
-# function(){
-#    ...
-# }
-
-# This type only do some command lines, without the need of input parameters
-# or to return something
